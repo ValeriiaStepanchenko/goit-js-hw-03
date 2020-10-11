b2bb27168ff446c78c5f754e456d6863dd48bc4f
ValeriiaStepanchenko <valeriya.stepanchenko@gmail.com>
2020-10-12 00:13:54 +0300

    5 tasks are completed

---
+++ task-3.js
@@ -0,0 +1,50 @@
+/*
+Поиск наибольшего значения среди свойств объекта
+Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает 
+имя самого продуктивного (который выполнил больше всех задач). 
+Сотрудники и кол-во выполненных задач содержатся как свойства объекта в формате "имя":"кол-во задач".
+*/
+
+const findBestEmployee = function (employees) {
+  "use strict";
+  let name = "";
+  let maxNumber = 0;
+
+  const arrOfEmployees = Object.entries(employees);
+
+  for (const employee of arrOfEmployees) {
+    if (employee[1] > maxNumber) {
+      maxNumber = employee[1];
+      name = employee[0];
+    }
+  }
+  return name;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99,
+};
+console.log(findBestEmployee(developers));
+// 'lorence'
+
+findBestEmployee(developers);
+const supports = {
+  poly: 12,
+  mango: 17,
+  ajax: 4,
+};
+console.log(findBestEmployee(supports));
+// 'mango'
+
+const sellers = {
+  lux: 147,
+  david: 21,
+  kiwi: 19,
+  chelsy: 38,
+};
+console.log(findBestEmployee(sellers));
+// 'lux'
---
+++ test.js
@@ -0,0 +1,6 @@
+const array = [
+  { name: "Artem", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+];
+
+console.log(array[0].price);
